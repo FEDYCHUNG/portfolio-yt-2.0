82a872cfc747bd80e909f3fb2544b73d2a5ff318
FEDYCHUNG <fedychung@gmail.com>
2022-10-07 13:36:52 +0700

    add react-social-icon

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,7 +1,15 @@
 import React from "react";
+import { SocialIcon } from "react-social-icons";
 
 type Props = {};
 
 export default function Header({}: Props) {
-  return <div>I am a Header</div>;
+  return (
+    <header>
+      <div>
+        {/* Social Icons */}
+        <SocialIcon url="https://www.youtube.com" />
+      </div>
+    </header>
+  );
 }
