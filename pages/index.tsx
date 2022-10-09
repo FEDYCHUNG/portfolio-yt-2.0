c56d3f30d5ebe797fdbc209f7ce34c642cee513a
FEDY <fedychung@gmail.com>
2022-10-09 13:53:25 +0700

    Add header

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@ import type { NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
 import Header from "../components/Header";
+import Hero from "../components/Hero";
 import styles from "../styles/Home.module.css";
 
 const Home: NextPage = () => {
@@ -13,10 +14,13 @@ const Home: NextPage = () => {
         {/* <link rel="icon" href="/favicon.ico" /> */}
       </Head>
 
-      {/* Header */}
       <Header />
 
       {/* Hero */}
+      <section id="hero">
+        <Hero />
+      </section>
+
       {/* About */}
       {/* Experience */}
       {/* Skill */}
