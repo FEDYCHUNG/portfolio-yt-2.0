557ff5496a9f6699f1abe9b60e533646d732d62e
FEDY <fedychung@gmail.com>
2022-10-29 19:31:45 +0700

    setting sanity backend

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
-import type { NextPage } from "next";
+import type { GetStaticProps } from "next";
 import Head from "next/head";
 import Image from "next/image";
+import Link from "next/link";
 import About from "../components/About";
 import ContactMe from "../components/ContactMe";
 import Header from "../components/Header";
@@ -8,9 +9,22 @@ import Hero from "../components/Hero";
 import Projects from "../components/Projects";
 import Skills from "../components/Skills";
 import WorkExperience from "../components/WorkExperience";
-import styles from "../styles/Home.module.css";
+import { Experience, PageInfo, Project, Skill, Social } from "../typings";
+import { fetchExperiences } from "../utils/fetchExperiences";
+import { fetchPageInfo } from "../utils/fetchPageInfo";
+import { fetchProjects } from "../utils/fetchProjects";
+import { fetchSkills } from "../utils/fetchSkills";
+import { fetchSocials } from "../utils/fetchSocials";
 
-const Home: NextPage = () => {
+type Props = {
+  pageInfo: PageInfo;
+  experiences: Experience[];
+  skills: Skill[];
+  projects: Project[];
+  socials: Social[];
+}
+
+const Home = ({ pageInfo, experiences, skills, projects, socials }: Props) => {
   return (
     <div className="bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory overflow-y-scroll overflow-x-hidden z-0 
     scrollbar scrollbar-track-gray-400/20 scrollbar-thumb-[#F7AB0A]/80" >
@@ -20,7 +34,7 @@ const Home: NextPage = () => {
         {/* <link rel="icon" href="/favicon.ico" /> */}
       </Head>
 
-      <Header />
+      <Header socials={socials} />
 
       {/* Hero */}
       <section id="hero" className="snap-start">
@@ -52,8 +66,36 @@ const Home: NextPage = () => {
       <section id="contact" className="snap-center">
         <ContactMe />
       </section>
+
+      <Link href="#hero">
+        <footer className="sticky bottom-5 w-full cursor-pointer">
+          <div className="flex items-center justify-center">
+            {/* <Image src="https://i.imgur.com/e2yvD6A.png" alt="" height={50} width={50}/> */}
+            {/* <img src="https://i.imgur.com/e2yvD6A.png" class="h-10 w-10 rounded-full filter grayscale hover:grayscale-0 cursor-pointer" alt="" /> */}
+          </div>
+        </footer>
+      </Link>
     </div>
   );
 };
 
 export default Home;
+
+
+export const getStaticProps: GetStaticProps<Props> = async () => {
+  const pageInfo: PageInfo = await fetchPageInfo();
+  const experiences: Experience[] = await fetchExperiences();
+  const skills: Skill[] = await fetchSkills();
+  const projects: Project[] = await fetchProjects();
+  const socials: Social[] = await fetchSocials();
+  return {
+    props: {
+      pageInfo,
+      experiences,
+      skills,
+      projects,
+      socials,
+    },
+    revalidate: 10, //regenerate page 10 second
+  }
+}
